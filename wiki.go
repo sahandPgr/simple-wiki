0368be774e811cb4f5a7c43f33c49abd3ccd7092
sahandMac <sahandmac@sahandMacs-MacBook-Pro.local>
2024-07-09 22:23:49 +0330

    ✅ Add view handler
    Add view, edit and save handler.

--- wiki.go
+++ wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -29,13 +29,45 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// renderTemplate renders a template to the response
+func renderTemplate(w http.ResponseWriter, templ string, p *Page) {
+	t, _ := template.ParseFiles(templ)
+	t.Execute(w, p)
+}
+
+// view handler
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><dive>%s</dive>", p.Title, p.Body)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	renderTemplate(w, "view", p)
+}
+
+// edit handler
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", viewHandler)
+	http.HandleFunc("/save", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
